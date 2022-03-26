6ac464e48db9da46c609b53d4100fc46baa9d728
Devibtissam <dev.ibtissamelhammoumy@gmail.com>
2022-03-26 18:35:31 +0100

    add the popup modal

--- app2.js
+++ app2.js
@@ -65,9 +65,15 @@ const fourGrid = [
 ];
 const gridSize = parseInt(localStorage.getItem('gridSize'));
 const cardsContainer = document.querySelector('.card-items');
+const timer = document.querySelector('.times');
+const modalContainer = document.querySelector('.modal-container');
+const modalText = document.querySelector('.modal-desc');
+console.log(modalContainer);
 const indexNums = [];
 let randNum = 0;
 
+//  generate Random Number to display cards dynamically from the arrays of each chosen Grid
+//  cards with same icon should not be displayed more than two times
 function getRandNumber(gridArr) {
   let lessThanTwo = false;
   while (!lessThanTwo) {
@@ -87,7 +93,7 @@ function getRandNumber(gridArr) {
     return randNum;
   }
 }
-
+// Displaying the shuffling Cards
 function DisplayGridItems(gridNum, gridArr) {
   let displayCard = [];
   for (let i = 0; i < gridNum; i++) {
@@ -98,11 +104,10 @@ function DisplayGridItems(gridNum, gridArr) {
     </div>`;
     displayCard.push(gridCard);
   }
-  //   console.log(displayCard);
   displayCard = displayCard.join('');
   cardsContainer.innerHTML = displayCard;
 }
-
+// check if the two selected cards are matched
 function checkCards(cardsName) {
   if (cardsName[0] === cardsName[cardsName.length - 1]) {
     // mathched card
@@ -111,13 +116,12 @@ function checkCards(cardsName) {
   // unmatched card
   return false;
 }
-// // @description game timer
+
 let second = 0;
 let minute = 0;
 let hour = 0;
-const timer = document.querySelector('.times');
-console.log(timer);
 
+// count the times while the user plays
 function startTimer() {
   timer.innerHTML = `${minute}mins ${second}secs`;
   second++;
@@ -141,62 +145,51 @@ function playGame() {
     DisplayGridItems(gridSize, fourGrid);
     cardsContainer.classList.add('four-grid');
   }
+  // counter to count the number of moves made by player
   let moves = 0;
-  const choosenCards = [];
+  const chosenCards = [];
   const unmatchedCards = [];
   const matchedCards = [];
   const flipCards = document.querySelectorAll('.flip-card');
   const moveCards = document.querySelector('.moves');
-  // console.log(moveCards);
-  // console.log(backCards);
+
   flipCards.forEach((card) => {
     card.addEventListener('click', (e) => {
-      // moves += 1;
-      // add moves each time you clicked on card
-      // console.log(card);
       const backCard = card.querySelector('.hide-card');
-      // console.log(backCard);
       if (backCard.classList.contains('hide-card')) {
-        // backCard.classList.remove('hide-card');
         backCard.classList.toggle('show-card');
-        // console.log('hhh');
         unmatchedCards.push(backCard);
       }
-      console.log(unmatchedCards);
       const selectedCard = e.currentTarget.dataset.title;
-      choosenCards.push(selectedCard);
-      if (choosenCards.length === 2) {
+      chosenCards.push(selectedCard);
+      if (chosenCards.length === 2) {
         moves += 1;
         moveCards.innerHTML = `moves: ${moves}`;
-        if (checkCards(choosenCards)) {
+        if (checkCards(chosenCards)) {
           flipCards.forEach((flipCard) => {
-            console.log(flipCard);
             if (
-              flipCard.dataset.title === choosenCards[0] ||
-              flipCard.dataset.title === choosenCards[choosenCards.length - 1]
+              flipCard.dataset.title === chosenCards[0] ||
+              flipCard.dataset.title === chosenCards[chosenCards.length - 1]
             ) {
               flipCard.classList.add('matched-card');
               matchedCards.push(flipCard);
-              console.log(matchedCards);
             }
           });
         } else {
-          // console.log('non opacity');
           unmatchedCards.forEach((unmatchedCard) => {
-            unmatchedCard.classList.add('unmatched-card');
+            unmatchedCard.classList.toggle('unmatched-card');
             setTimeout(() => {
-              // unmatchedCard.classList.add('hide-card');
               unmatchedCard.classList.remove('show-card');
             }, 1100);
           });
         }
-        choosenCards.length = 0;
+        chosenCards.length = 0;
         unmatchedCards.length = 0;
         if (matchedCards.length === gridSize) {
-          // timer.innerHTML = '0 mins 0 secs';
-          // console.log('hhhhh');
+          // stop the timmer and display the pop-up modal at the end of the game
           clearInterval(interval);
-          // console.log(interval);
+          modalText.innerHTML = `you completed the game using ${moves} moves in ${minute} mins ${second} secs`;
+          modalContainer.classList.add('open-modal');
         }
       }
     });
